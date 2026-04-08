37b4e0b2bc7ebe8276c33e5997d8e6b5cfc0b4c0
piggie <matvey00014@gmail.com>
2026-04-08 10:11:02 +0500

    file parser init

--- parser/parser.c
+++ parser/parser.c
@@ -1,14 +1,22 @@
 #include "parser.h"
 
-bencode_pairs_list parse_file_content_buffer(file_content_buffer buffer)
+bencode_pairs parse_file_content_buffer(file_content_buffer buffer)
 {
-    bencode_pairs_list pairs_list;    
+    bencode_pairs pairs_list = {0};    
     int i = 0;
     while(i < buffer.size)
     {
         switch (buffer.data[i])
         {
-            case 'd':
+            case 'd': pairs_list.bencode_pairs[pairs_list.count++] = parse_dict(buffer);
+                      break;
+            case 'i': pairs_list.bencode_pairs[pairs_list.count++] = parse_num(buffer);
+                      break;
+            case 'l': pairs_list.bencode_pairs[pairs_list.count++] = parse_list(buffer);
+                      break;
+            case '1': case '2': case '3': case '4': case '5':
+            case '6': case '7': case '8': case '9':
+                      pairs_list.bencode_pairs[pairs_list.count++] = parse_string(buffer)
         }
     }
 }
--- parser/parser.h
+++ parser/parser.h
@@ -1,3 +1,4 @@
+#include <stddef.h>
 #include <stdint.h>
 #include "../types/types.h"
 
@@ -5,6 +6,7 @@ typedef enum {
     BENCODE_LIST,
     BENCODE_NUMBER,
     BENCODE_STRING,
+    BENCODE_DICT,
 } bencode_type;
 
 typedef struct bencode_value bencode_value;
@@ -15,22 +17,36 @@ typedef struct {
     bencode_value *value;
 } bencode_pair;
 
+typedef struct {
+    bencode_pair *bencode_pairs;
+    int count;
+} bencode_pairs;
+
+typedef struct {
+    char *data;
+    size_t length;
+} string;
+
+typedef struct {
+    bencode_pairs pairs;
+    file_content_buffer buffer;
+    int cursor;
+} bencode_cursor;
+
 struct bencode_value {
     bencode_type type;
     union {
         int64_t number;
-        char *string;
+        string string;
         struct {
             bencode_value **items;
             int count;
         } list;
+        bencode_pairs dict;
     };
 };
 
 
-typedef struct {
-    bencode_pair *bencode_pairs;
-    int size;
-} bencode_pairs_list;
 
-bencode_pairs_list parse_file_content_buffer(file_content_buffer buffer);
+
+bencode_pairs parse_file_content_buffer(file_content_buffer buffer);
