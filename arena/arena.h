395fe2ee9648722008c0afb64f02860109680d23
piggie <matvey00014@gmail.com>
2026-05-21 01:04:42 +0500

    arena implementation almost done need to test

--- arena/arena.h
+++ arena/arena.h
@@ -6,6 +6,10 @@
 #define MB(x) (KB(x) * 1024)
 #define GB(x) (MB(x) * 1024)
 
+
+#define COMMIT_CHUNK KB(64)
+
+
 typedef struct
 {
     uint8_t *memory;
@@ -15,6 +19,13 @@ typedef struct
 } Arena;
 
 Arena arena_create(size_t reserve_size);
+void *arena_push(Arena *a, size_t size);
+void arena_reset(Arena *a);
+void arena_rewind(Arena *a);
+void arena_destroy(Arena *a);
+
+#define push_array(arena, type, count)  (type *)arena_push((arena), sizeof(type)*(count))
+#define push_struct(arena, type) push_array((arena), (type), 1)
 
 #endif // !STB_ARENA_H
 
@@ -47,5 +58,41 @@ Arena arena_create(size_t reserve_size)
     a.memory = (uint8_t *)os_reserve(reserve_size);
     a.reserved = reserve_size;
     return a;
+}
+
+void *arena_push(Arena *a, size_t size)
+{
+    size_t new_commit;
+    void *ptr;
+
+    if (a->used + size > a->commited) 
+    {
+        new_commit = a->commited + COMMIT_CHUNK;
+        assert(a->used + size <= a->reserved && "Arena out of reserved space");
+        os_commit(a->memory, new_commit);
+        a->commited = new_commit;
+    }
+
+    ptr = a->memory + a->used;
+    a->used += size;
+
+    return ptr;
+
+}
+
+void arena_rewind(Arena *a)
+{
+    a->used = 0;
+}
+
+void arena_reset(Arena *a)
+{
+    a->used = 0;
+    os_commit(a->memory, a->used);
+}
+void arena_destroy(Arena *a)
+{
+    os_release(a->memory, a->reserved);
+    *a = (Arena){0};
 }
 #endif // ARENA_IMPLEMENTATION
